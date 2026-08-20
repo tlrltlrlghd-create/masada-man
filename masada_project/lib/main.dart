da33bf86a1d0ed3eb2b44744d9b1d19b4befd9b7
tlrltlrlghd-create <tlrltlrlghd@gmail.com>
2026-08-20 23:12:32 +0900

    Update main.dart

--- masada_project/lib/main.dart
+++ masada_project/lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:just_audio/just_audio.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -79,7 +79,7 @@ class _MasadaDashboardAppState extends State<MasadaDashboardApp> {
   }
 
   Future<void> _initApp() async {
-    await _audioPlayer.setLoopMode(LoopMode.one);
+    _audioPlayer.setReleaseMode(ReleaseMode.loop);
 
     _startTimers();
     await _requestPermissions();
@@ -159,13 +159,13 @@ class _MasadaDashboardAppState extends State<MasadaDashboardApp> {
     String assetPath = '';
     switch (newSound) {
       case EngineSoundType.v8Engine:
-        assetPath = 'assets/sounds/v8.mp3';
+        assetPath = 'sounds/v8.mp3';
         break;
       case EngineSoundType.ioniq5n:
-        assetPath = 'assets/sounds/ioniq5n.mp3';
+        assetPath = 'sounds/ioniq5n.mp3';
         break;
       case EngineSoundType.porscheEsound:
-        assetPath = 'assets/sounds/porsche.mp3';
+        assetPath = 'sounds/porsche.mp3';
         break;
       case EngineSoundType.mute:
         break;
@@ -173,9 +173,9 @@ class _MasadaDashboardAppState extends State<MasadaDashboardApp> {
 
     try {
       await _audioPlayer.stop();
-      await _audioPlayer.setAsset(assetPath);
-      await _audioPlayer.setSpeed(1.0);
-      await _audioPlayer.play();
+      await _audioPlayer.setSource(AssetSource(assetPath));
+      await _audioPlayer.setPlaybackRate(1.0);
+      await _audioPlayer.resume();
     } catch (_) {}
   }
 
@@ -200,7 +200,7 @@ class _MasadaDashboardAppState extends State<MasadaDashboardApp> {
     _lastAppliedPitch = smoothedPitch;
 
     try {
-      await _audioPlayer.setSpeed(smoothedPitch);
+      await _audioPlayer.setPlaybackRate(smoothedPitch);
     } catch (_) {}
   }
 
